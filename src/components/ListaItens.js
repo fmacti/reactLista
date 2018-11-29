d74e45054501357f1558c8153a8aaca8e78d6b0b
fmacti <fer.aeq@gmail.com>
2018-11-29 14:02:19 -0200

    App4

---
+++ src/components/ListaItens.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  ScrollView,
+  StyleSheet
+} from 'react-native';
+import axios from 'axios';
+import Itens from './Itens';
+
+export default class ListaItens extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { listaItens: [] };
+    }
+
+    componentWillMount() {
+        //requisição HTTP
+        axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+             .then(response => { this.setState({ listaItens: response.data }); })
+             .catch(() => { console.log('Erro ao recuperar os dados'); });
+    }
+   
+    render() {        
+        return (
+            <ScrollView style={styles.fundo}>   
+                { this.state.listaItens.map(item => (<Itens key={item.titulo} item={item} />)) }
+            </ScrollView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    fundo: {
+        backgroundColor: '#DDD'
+    },
+});
